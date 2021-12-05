0457271914ba37c7d517e170a095018fea50c4f3
WolfDen133 <61922721+WolfDen133@users.noreply.github.com>
2021-12-05 22:20:55 +0000

    Add ua lang

--- src/WolfDen133/WFT/Lang/LanguageManager.php
+++ src/WolfDen133/WFT/Lang/LanguageManager.php
@@ -9,7 +9,7 @@ class LanguageManager
 {
     private WFT $plugin;
     private string $dataPath;
-    public array $internalLanguages = ['en', 'ru'];
+    public array $internalLanguages = ['en', 'ru', 'ua'];
 
     public string $defaultLanguage = 'en';
     public Language $selectedLanguage;
