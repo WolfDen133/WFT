5c8bc635ca0a2616bbcd8df316decf7b878a8349
WolfDen133 <61922721+WolfDen133@users.noreply.github.com>
2021-12-07 21:46:27 +0000

    Update LanguageManager.php

--- src/WolfDen133/WFT/Lang/LanguageManager.php
+++ src/WolfDen133/WFT/Lang/LanguageManager.php
@@ -9,7 +9,7 @@ class LanguageManager
 {
     private WFT $plugin;
     private string $dataPath;
-    public array $internalLanguages = ['en', 'ru', 'ua', 'ge', 'sp'];
+    public array $internalLanguages = ['en', 'ru', 'ua', 'ge', 'sp', 'sk'];
 
     public string $defaultLanguage = 'en';
     public Language $selectedLanguage;
